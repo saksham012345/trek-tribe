32bb2dda824d023f7d32bd6a25da5587346e2cee
Saksham012345 <tanejasaksham384@gmail.com>
2025-12-13 20:12:06 +0530

    Fxing frontend

--- web/src/pages/OrganizerRouteOnboarding.tsx
+++ web/src/pages/OrganizerRouteOnboarding.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import api from '../config/api';
 import { useAuth } from '../contexts/AuthContext';
 import { useToast } from '../components/ui/Toast';
-import Tooltip from '../components/ui/Tooltip';
+import { Tooltip } from '../components/ui/Tooltip';
 import { Skeleton } from '../components/ui/Skeleton';
 
 interface StatusResponse {
